f588cf6a30675936767a8a881b0d63af23681997
mr-zerone05 <tuansmhtbt@gmail.com>
2025-11-12 10:52:07 +0700

    CHUAN BI DEPLOY

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,17 +3,32 @@ import taskRoutes from './routes/taskRoutes.js';
 import { connectDB } from './config/db.js'; 
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 
 const port = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173' }));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: 'http://localhost:5173' }));
+}
+
 
 app.use("/api/tasks",taskRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+}
 
 connectDB().then(() => {
   app.listen(port, () => {
